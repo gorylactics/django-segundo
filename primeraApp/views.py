05ae215ad9e074a76c58be569ac47aff1a9315c0
Adrian <adrian.miranda.r@gmail.com>
2021-07-30 21:30:07 -0400

    segundo metodo de mostrar hora

--- primeraApp/views.py
+++ primeraApp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, HttpResponse
 import datetime
+import time
 
 def index(request):
     fecha = {
-        'fecha' : datetime.datetime.now(),
+        'fecha' : datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'fecha2' : time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
     }
     return render(request, 'index.html' , fecha)
